e868234b3be56d928591245d3443a8c5336ca4fe
helenamarkus14 <helenamarkus14@gmail.com>
2022-03-22 19:54:20 -0600

    set up

--- pandacollector/settings.py
+++ pandacollector/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 from dotenv import load_dotenv
+
+load_dotenv()
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
